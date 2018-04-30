d4e5f7551341853581c1727f24cf669b3409756f
Elie Maamary <eliemaamary17@gmail.com>
2018-04-30 19:04:49 +0300

    removed .class files

--- src/test/org/digitalstain/datrie/run.sh
+++ src/test/org/digitalstain/datrie/run.sh
@@ -1,2 +1,3 @@
 javac -cp /home/tsugiru/Documents/EECE437_indiv_proj/DoubleArrayTrie/src/test/:/home/tsugiru/Documents/EECE437_indiv_proj/DoubleArrayTrie/src/main/ TestDoubleArrayTrieWrapper.java
 java -cp /home/tsugiru/Documents/EECE437_indiv_proj/DoubleArrayTrie/src/test/:/home/tsugiru/Documents/EECE437_indiv_proj/DoubleArrayTrie/src/main/ org.digitalstain.datrie.TestDoubleArrayTrieWrapper
+rm /home/tsugiru/Documents/EECE437_indiv_proj/DoubleArrayTrie/src/test/org/digitalstain/datrie/*.class /home/tsugiru/Documents/EECE437_indiv_proj/DoubleArrayTrie/src/main/org/digitalstain/datrie/*.class
